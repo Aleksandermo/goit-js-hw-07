cd0a60e867b0da1e0034aaac7aba8995b660554b
Aleksandermo <143647602+Aleksandermo@users.noreply.github.com>
2024-01-29 15:19:35 +0100

    feat: init1 jshw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElement = document.querySelector('.gallery');
+  const allGalleryItems = galleryItems.map(image => 
+    `<li class = "gallery_item">
+    <a class = "gallery_link" href = "${image.original}">
+    <img
+        class="gallery__image"
+        src="${image.preview}" 
+        alt="${image.description}"
+        data-source="${image.original}"
+    >
+    </img>
+    </a>
+    </li>`
+  ).join('');
+galleryElement.insertAdjacentHTML('afterbegin', allGalleryItems); 
+
+let lightboxInstance = null;
+
+galleryElement.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const imageUrl = event.target.dataset.source;
+
+    if (lightboxInstance) {
+        lightboxInstance.close();
+    }
+
+    lightboxInstance = basicLightbox.create(`
+    <img src="${imageUrl}" alt="Image description">
+    `);
+
+    lightboxInstance.show();
+
+// Zamykanie okna z klawiatury
+document.addEventListener('keydown', onKeyPress);
+}
+
+function onKeyPress(event) {
+    if (event.key === 'Escape' && lightboxInstance) {
+        lightboxInstance.close();
+        lightboxInstance = null;
+        document.removeEventListener('keydown', onKeyPress);
+    }
+}
